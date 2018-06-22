f6417caa6ec91958d0e0646078bfffe569d64808
wjpatel <40117008+wjpatel@users.noreply.github.com>
2018-06-21 21:54:58 -0500

    Functional improvements
    Made inputs more responsive, prevent game from being started again when already running
    Used "let" instead of "var" for most variables

--- GameOfLife.js
+++ GameOfLife.js
@@ -5,14 +5,23 @@ function GameGrid(){
   this.grid = [];
 }
 
+GameGrid.prototype.getCellClassValue = function(stateNumberValue){
+	// The class of the td element is used to control its color
+	// to indicate alive or dead
+	 if (stateNumberValue == 1) {
+	 	// it's alive!
+	 	return "alive";
+	 } else {
+	 	return "dead";
+	 }
+}
+
 GameGrid.prototype.initializeGrid= function(rows, columns){
 //function initializeGrid(rows, columns){
 	this.grid = [];
-	var r;
-	for (r=0; r<rows; r++) {
-	 var curColumn = [];
-	 	var c;
-	 	for(c=0; c<columns; c++) {
+	for (let r=0; r<rows; r++) {
+	 let curColumn = [];
+	 	for(let c=0; c<columns; c++) {
 	 		curColumn.push(0);
 	 	}
 	 	this.grid.push(curColumn);
@@ -20,12 +29,14 @@ GameGrid.prototype.initializeGrid= function(rows, columns){
 }
 
 GameGrid.prototype.flipGridValue= function(row, column){
+	// Default make it die
+	let newValue = 0;
 	if(this.grid[row][column] == 0) {
-		this.grid[row][column] = 1;	
-	} else {
-		this.grid[row][column] = 0;
-	}
-	this.renderGrid();
+		// Make it live
+		newValue = 1;
+	} 
+	this.grid[row][column] = newValue;	
+	return newValue;
 }
 
 
@@ -36,47 +47,45 @@ GameGrid.prototype.getGrid= function(){
 GameGrid.prototype.renderGrid= function(){
 
 	//console.log(this.grid.length);
-	var gameTableHTML = document.getElementById("gameTable");
+	let gameTableHTML = document.getElementById("gameTable");
 	gameTableHTML.innerHTML = "";
-	for (r=0; r<this.grid.length; r++) {
-		var rowElement = document.createElement("tr");
+	for (let r=0; r<this.grid.length; r++) {
+		let rowElement = document.createElement("tr");
 		//console.log(rowElement);
 		gameTableHTML.appendChild(rowElement);
-	 	var c;
-	 	for(c=0; c<this.grid[r].length; c++) {
-	 		var cellState = this.grid[r][c];
-	 		var columnElement = document.createElement("td");
+	 	for(let c=0; c<this.grid[r].length; c++) {
+	 		let cellState = this.grid[r][c];
+	 		let columnElement = document.createElement("td");
 
 	 		//columnElement.innerHTML = cellState;
 
 	 		// set the class of the td element to control its color
 	 		// rather than having innerHTML of 0 or 1
-	 		var cellClass = "dead";
-	 		if (cellState == 1) {
-	 			// it's alive!
-	 			cellClass = "alive";
-	 		}
-	 		columnElement.setAttribute("class", cellClass);
+	 		columnElement.setAttribute("class", this.getCellClassValue(cellState));
 	 		
-	 		var cellAtt = document.createAttribute("id");
-	 		var cellId = "cell-" + r + "-" + c;
+	 		let cellAtt = document.createAttribute("id");
+	 		let cellId = "cell-" + r + "-" + c;
 	 		cellAtt.value = cellId;
 	 		columnElement.setAttributeNode(cellAtt);
 	 		rowElement.appendChild(columnElement);
 
 			document.getElementById(cellId).addEventListener("click", function(e){
-				var clickedCellId = e.target.getAttribute("id");
+				let clickedCellId = e.target.getAttribute("id");
 				//console.log("you clicked " + clickedCellId);
 				
 				// determing row and column value based on id format "cell-r-c"
-				var cellIdSegments = clickedCellId.split("-");
-				var clickedCellRow = cellIdSegments[1];
-				var clickedCellColumn = cellIdSegments[2];
+				let cellIdSegments = clickedCellId.split("-");
+				let clickedCellRow = cellIdSegments[1];
+				let clickedCellColumn = cellIdSegments[2];
 				//console.log("row: " + clickedCellRow);
 				//console.log("column: " + clickedCellColumn);
 
 				//update grid
-				gameOfLife.flipGridValue(clickedCellRow, clickedCellColumn);
+				let newGridValue = gameOfLife.flipGridValue(clickedCellRow, clickedCellColumn);
+
+				//Change cell class		
+				e.target.setAttribute("class", gameOfLife.getCellClassValue(newGridValue));
+				
 			}, false);
 	 	}
 
@@ -85,24 +94,24 @@ GameGrid.prototype.renderGrid= function(){
 
 GameGrid.prototype.tickGeneration= function(){
 	//Copy grid for reference
-	var gridCopy = [];
-	for (r=0; r<this.grid.length; r++) {
+	let gridCopy = [];
+	for (let r=0; r<this.grid.length; r++) {
 		gridCopy.push(this.grid[r].slice());
 	}
 
 	//Track whether any live cells were found
-	var lifeDiscovered = false;
+	let lifeDiscovered = false;
 	//Loop through reference grid and update game grid
-	for (r=0; r<gridCopy.length; r++) {
-		for(c=0; c<gridCopy[r].length; c++) {
+	for (let r=0; r<gridCopy.length; r++) {
+		for(let c=0; c<gridCopy[r].length; c++) {
 			//calculate number of live neighbors
-			var liveNeighbors = 0;
+			let liveNeighbors = 0;
 			// for nrp (neighbor row positions) -1 to 1 relative to current cell
-			for (nrp=r-1; nrp<=r+1; nrp++) {
+			for (let nrp=r-1; nrp<=r+1; nrp++) {
 				// if this row index is within the grid
 				if(nrp >= 0 && nrp < gridCopy.length){
 					// for ncp (neighbor column positions) -1 to 1 relative to current cell
-					for(ncp=c-1; ncp<=c+1; ncp++) {
+					for(let ncp=c-1; ncp<=c+1; ncp++) {
 						// if this column index is within the grid and 
 						if(ncp >= 0 && ncp < gridCopy[r].length){
 							// it is not the current cell being evalutated
@@ -131,7 +140,6 @@ GameGrid.prototype.tickGeneration= function(){
 					//Any live cell with more than three live neighbors dies, as if by overpopulation.
 					this.grid[r][c] = 0;
 				} 
-				
 				//Any live cell with two or three live neighbors lives on to the next generation.
 				
 			} else {
@@ -152,7 +160,7 @@ GameGrid.prototype.tickGeneration= function(){
 	// Stop game if no living cells found
 	if(!lifeDiscovered){
 		gameStopped = true;
-		document.getElementById("endOfGameMessage").innerHTML = "Life is extinct.";
+		document.getElementById("gameStatusMessage").innerHTML = "Life is extinct.";
 	}
 }
 
@@ -163,16 +171,25 @@ function registerEvents(){
 }
 
 function getSizeInputs(){
-	var inputRows = document.gameSize.nRows.value;
-	var inputColumns = document.gameSize.nColumns.value;
+	let inputRows = document.gameSize.nRows.value;
+	let inputColumns = document.gameSize.nColumns.value;
 	gameOfLife.initializeGrid(inputRows,inputColumns);
 //	console.log(grid);
 	gameOfLife.renderGrid();
+
+	document.getElementById("instruction2").removeAttribute("hidden");
+	document.getElementById("startGame").removeAttribute("hidden");
+	document.getElementById("stopGame").removeAttribute("hidden");
+	document.getElementById("gameStatusMessage").innerHTML = "Start when ready.";
+	//.removeAttribute("hidden");
 }
 
 function startGame(){
 	gameStopped = false;
-	document.getElementById("endOfGameMessage").innerHTML = " ";
+	document.getElementById("gameStatusMessage").innerHTML = "Running simulation.";
+	// Make Stop button active
+	document.getElementById("startGame").disabled = "disabled";
+	document.getElementById("stopGame").disabled = false;
 	runGame();
 }
 
@@ -180,9 +197,14 @@ function runGame(){
 	if(!gameStopped){
 		gameOfLife.tickGeneration();
 		setTimeout(runGame,1000);
+	} else {
+		// Make Start button active
+		document.getElementById("startGame").disabled = false;
+		document.getElementById("stopGame").disabled = "disabled";
 	}
 }
 
 function stopGame(){
 	gameStopped = true;
+	document.getElementById("gameStatusMessage").innerHTML = "Game has been stopped.";
 }
